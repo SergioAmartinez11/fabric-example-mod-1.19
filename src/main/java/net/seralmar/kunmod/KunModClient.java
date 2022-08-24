8dfc475de7f2dec1e8fcc535a7678077853d9341
Sergio <alonso.martinez64@uabc.edu.mx>
2022-08-23 21:22:50 -0700

    First commit for the kunmod project

---
+++ src/main/java/net/seralmar/kunmod/KunModClient.java
@@ -0,0 +1,10 @@
+package net.seralmar.kunmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class KunModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
